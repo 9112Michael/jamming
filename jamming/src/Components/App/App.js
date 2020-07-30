022889c7d321793a554fb2c1ce0c9dd16cf940f7
9112Michael <sitkabikemike@yahoo.com>
2020-07-30 15:53:00 -0700

    Create method to save playlist to user account

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -25,6 +25,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -48,6 +49,10 @@ class App extends React.Component {
     this.setState({ playlistName: name });
   }
 
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+  }
+
   render() {
     return (
       <div>
@@ -59,7 +64,8 @@ class App extends React.Component {
       <Playlist playlistName={this.state.playlistName}
                 playlistTracks={this.state.playlistTracks}
                 onRemove={this.removeTrack}
-                onNameChange={this.updatePlaylistName} />
+                onNameChange={this.updatePlaylistName}
+                onSave={this.savePlaylist} />
     </div>
   </div>
 </div>
